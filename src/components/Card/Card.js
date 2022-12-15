5956404b5f0198ce28ed60540695afdb0ad529e6
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-12-14 23:27:19 -0600

    feat: add a card component and its corresponding style file

---
+++ src/components/Card/Card.js
@@ -0,0 +1,57 @@
+import { LitElement, html, css } from "lit-element";
+import "@/components/ImageContainer";
+import "@/components/Chip";
+import "@/components/Button";
+import { cardStyles } from "@/components/Card/CardStyles";
+
+export class Card extends LitElement {
+	static get properties() {
+		return {
+			img: { type: String },
+			title: { type: String },
+			description: { type: String },
+			tags: { type: Array },
+		};
+	}
+
+	static get styles() {
+		return cardStyles;
+	}
+
+	constructor() {
+		super();
+		this.img = "";
+		this.title = "";
+		this.description = "";
+		this.tags = [];
+	}
+
+	render() {
+		return html`
+			<!-- https://lit.dev/docs/v1/components/styles/#customprops -->
+			<style>
+				image-container {
+					--figure-aspect-ratio: 4/3;
+				}
+			</style>
+
+			<article>
+				<image-container src="${this.img}"></image-container>
+				<section>
+					<h1>${this.title}</h1>
+					<div>
+						${this.tags.map((tag) => {
+							return html`
+								<chip-lit tagText="${tag}"></chip-lit>
+							`;
+						})}
+					</div>
+					<p>${this.description}</p>
+					<button-lit text="save"></button-lit>
+				</section>
+			</article>
+		`;
+	}
+}
+
+customElements.define("card-lit", Card);
---
+++ src/components/Card/CardStyles.js
@@ -0,0 +1,62 @@
+import { css } from "lit-element";
+
+export const cardStyles = css`
+	article {
+		width: 100%;
+		height: 100%;
+		overflow: hidden;
+		border-radius: 10px;
+		display: flex;
+		flex-flow: column nowrap;
+		align-items: center;
+		justify-content: center;
+
+		transition: box-shadow 300ms;
+	}
+
+	article:hover {
+		box-shadow: 0px 0px 50px 10px #111;
+	}
+
+	h1,
+	p {
+		padding: 0;
+		margin: 0;
+	}
+
+	h1 {
+		color: #fff;
+		font-size: 1.5rem;
+	}
+
+	p {
+		color: #bbb;
+		font-size: 0.9rem;
+		font-size: 1rem;
+		margin-bottom: 1rem;
+	}
+
+	div {
+		display: flex;
+		flex-flow: row wrap;
+		align-items: center;
+		justify-content: flex-start;
+		gap: 1rem;
+	}
+
+	section {
+		box-sizing: border-box;
+		width: 100%;
+		display: flex;
+		flex-flow: column nowrap;
+		gap: 1rem;
+		background-color: #222;
+		padding: 1rem;
+		flex-grow: 1;
+		flex-basis: auto;
+	}
+
+	button-lit {
+		margin-top: auto;
+	}
+`;
