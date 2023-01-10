41839413330adeefe1c55d52df47d3b4459dfc62
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2023-01-10 12:06:07 -0600

    feat: WIP - smooth transition between img change

--- src/components/ImageContainer/ImageContainer.js
+++ src/components/ImageContainer/ImageContainer.js
@@ -11,16 +11,55 @@ export class ImageContainer extends LitElement {
 	constructor() {
 		super();
 		this.src = "/public/img/land-1.jpg";
+
+		this.srcObserver = new MutationObserver(
+			this.#srcObserverCallback.bind(this)
+		);
+	}
+
+	firstUpdated() {
+		this.srcObserver.observe(this.#imgElement, { attributes: true });
+	}
+
+	disconnectedCallback() {
+		super.disconnectedCallback();
+		this.srcObserver.disconnect();
+	}
+
+	#srcObserverCallback(changes) {
+		changes.forEach((change) => {
+			change.attributeName.includes("src") && this.handleLoadStart();
+		});
 	}
 
 	static get styles() {
 		return imageContainerStyles;
 	}
 
+	get #imgElement() {
+		return this.shadowRoot.querySelector("img");
+	}
+
+	/**
+	 * this method is called every time a img finish
+	 * its src loading
+	 */
+	handleLoad() {
+		console.log("loading complete");
+	}
+
+	handleLoadStart() {
+		console.log("loading init");
+	}
+
 	render() {
 		return html`
 			<figure>
-				<img src="${this.src}" />
+				<img
+					src="${this.src}"
+					loading="lazy"
+					@load=${this.handleLoad}
+				/>
 			</figure>
 		`;
 	}
