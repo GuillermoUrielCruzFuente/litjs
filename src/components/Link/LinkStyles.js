e8a892afdf89b080b6db93fa405ce6aae71596fb
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-03-19 21:59:27 -0600

    feat: improve link styles

--- src/components/Link/LinkStyles.js
+++ src/components/Link/LinkStyles.js
@@ -1,13 +1,18 @@
 import { css } from "lit-element";
 
 export const linkStyles = css`
+	:host {
+		--link-size: 1rem;
+	}
+
 	a {
-		font-size: 1rem;
+		font-size: var(--link-size);
 		font-weight: 900;
-		text-decoration: none;
 		text-transform: uppercase;
 		color: #999;
 		transition: 200ms color;
+		text-decoration: underline;
+		text-decoration-thickness: 3px;
 	}
 
 	a:hover {
