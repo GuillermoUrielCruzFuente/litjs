09578d28d32e5c5657627039cbc9585c231521c3
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-03-19 22:04:46 -0600

    feat: add home dependencies

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import "@/components/CardsLayout";
+//global styles for page
 import "@/styles/styles.scss";
+
+//Home components
+import "@/components/CardsLayout";
 import "@/components/Link";
+import "@/components/Header";
+import "@/components/HeaderItemsContainer";
